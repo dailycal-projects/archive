13f1c27d0fa9d6a3fe8fc34c4221c7984e3f5a8c
vv-qian <v.v.qian@gmail.com>
2018-12-16 13:52:29 -0800

    fixing bug: properly updating sponsorship

--- archive/management/commands/createmonths.py
+++ archive/management/commands/createmonths.py
@@ -12,10 +12,11 @@ class Command(BaseCommand):
     help = "Populate database with month objects."
 
     def handle(self, *args, **options):
-    	current_year = datetime.now().year
-        for year in range(1900, current_year + 1):
+        current_year = datetime.now().year
+        for year in range(1875, current_year + 1):
             for month in range(1, 13):
                 start_date = date(year, month, 1)
                 month_obj, c = Month.objects.get_or_create(date=start_date)
+                Month.objects.filter(date=start_date).update(sponsor=None)
                 end_date = start_date + timedelta(weeks=4)
                 pages = Page.objects.filter(date__range = (start_date, end_date))
